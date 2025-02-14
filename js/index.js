097d57a2045c388d6ae4840044c690cad053bc17
EricDs6 <vieiraeric0304@gmail.com>
2025-02-13 21:11:26 -0400

    Primeiro commit

---
+++ js/index.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('a[href^="#"]');
+    const mobileMenuButton = document.querySelector('.mobile-menu');
+    const navLinks = document.querySelector('.nav-links');
+
+    for (const link of links) {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    }
+
+    mobileMenuButton.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
