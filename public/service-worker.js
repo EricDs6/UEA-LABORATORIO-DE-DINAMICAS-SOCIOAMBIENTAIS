574875ec92338e2ce1b2158ef2dd58fac40f3bae
EricDs6 <vendasec0304@gmail.com>
2025-03-20 10:28:52 -0400

    mobile correção

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,19 @@
 self.addEventListener('install', event => {
+    self.skipWaiting(); // Força a ativação imediata
     console.log('ServiceWorker instalado');
     // Adicione arquivos ao cache se necessário
 });
 
 self.addEventListener('activate', event => {
+    caches.keys().then(cacheNames => {
+        return Promise.all(
+            cacheNames.map(cache => {
+                if (cache !== 'nome-do-novo-cache') {
+                    return caches.delete(cache);
+                }
+            })
+        );
+    });
     console.log('ServiceWorker ativado');
     // Limpe caches antigos se necessário
 });
